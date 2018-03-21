8249256450634a0d968d4391bae7e32fba653532
Fabripink <37373367+Fabripink@users.noreply.github.com>
2018-03-21 09:26:45 -0500

    tercer commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+ #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x=8;
+    float y=3;
+    int s=x+y;
+    int r=x-y;
+    int m=x*y;
+    float d=x/y;
+    int a,b;
+    cin >>a;
+    cin >>b;
+
+    std::cout <<"el primer numero es: "<<x<<std::endl;
+    std::cout <<"el segundo numero es: "<<y<<std::endl;
+    std::cout <<"la suma es: "<<s<<std::endl;
+    std::cout <<"la resta es: "<<r<<std::endl;
+    std::cout <<"la multiplicacion es: "<<m<<std::endl;
+    std::cout <<"la division es: "<<d<<std::endl;
+    std::cout <<"a es: "<<a<<" y b es: "<<b<<std::endl;
+    return 0;
+}
