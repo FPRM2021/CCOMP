c73b93f9f0469de8d880a41d5588d55c75218a43
Fabripink <37373367+Fabripink@users.noreply.github.com>
2018-03-27 09:54:00 -0500

    septimo commit

--- main.cpp
+++ main.cpp
@@ -21,7 +21,81 @@ int main()
     std::cout <<"ingrese el radio del circulo: "<<std::endl;
     cin>>a;
     float c=(3.14)*(a*a);
-    cout<<"el area es: "<<c;*/
-    cout<<"*********\t***\t\t*\t\t*\n*\t*";
+    cout<<"el area es: "<<c;
+    cout<<"*********\t **\t\t*\t\t*\n*\t*      *    *\t       ***\t       * *\n*\t*     *      *\t      *****\t      *   *\n*\t*     *      *\t     *******\t     *     *\n*\t*      *    *\t        *\t    *       *\n*********\t **\t\t*\t   ***********";*/
+    /*int p=7;
+    int r=2;
+    int q=6;
+    int w=4;
+    int x=5;
+    int y=2;
+    int z=p*r%q+w/x-y;*/
+    /*cin>>a;
+    cin>>b;
+    cin>>c;
+    if (a>b and a>c){
+        cout<<a;
+    }else{
+    if(b>a and b>c){
+        cout<<b;
+    }else{
+        cout<<c;
+    }
+    }*/
+
+    /*int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cin>>e;
+    if (a>b and a>c and a>d and a>e){
+        cout<<a;
+    }else{
+        if(b>a and b>c and b>d and b>e){
+        cout<<b;}
+        else{
+        if(c>a and c>b and c>d and c>e){
+        cout<<c;}
+        else{
+        if(d>a and d>b and d>c and d>e){
+        cout<<d;}
+        else{
+        if(e>a and e>b and e>c and e>d){
+        cout<<e;}}}}}
+        if (a>b and a>c and a>d and a>e){
+        cout<<" "<<a;
+    }else{
+        if(b<a and b<c and b<d and b<e){
+        cout<<" "<<b;}
+        else{
+        if(c<a and c<b and c<d and c<e){
+        cout<<" "<<c;}
+        else{
+        if(d<a and d<b and d<c and d<e){
+        cout<<" "<<d;}
+        else{
+        if(e<a and e<b and e<c and e<d){
+        cout<<" "<<e;}}}}}*/
+    /*int n;
+    cin>>n;
+    if (n%2){
+        cout<<"impar";
+    }else{
+    cout<<"par";
+    }*/
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+    if (a%b){
+        cout<<"no es multiplo";
+    }else{
+        cout<<"si es multiplo";
+    }
     return 0;
 }
