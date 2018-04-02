f85dc4458b63fb26e91c49de95dffaf9b7ff1ab7
Fabripink <37373367+Fabripink@users.noreply.github.com>
2018-04-01 21:40:00 -0500

    segunda subida

--- tarea1.cpp
+++ tarea1.cpp
@@ -14,6 +14,7 @@ int main()
             cout<<"es mayor de edad";
         }*/
     /*segundo ejercicio*/
+
     /*int n;
     cout<<"introduzca un numero ";
     cin>>n;
@@ -24,6 +25,7 @@ int main()
             cout<<i;
             }
     }*/
+
     /*tercer ejercicio*/
     /*int a;
     int b;
@@ -48,6 +50,7 @@ int main()
     }
     int promedio=(a+b+c)/3;
     cout<<"el promedio es: "<<promedio;*/
+
     /*cuarto ejercicio*/
     /*int a,b;
     cout<<"introduzca dos numeros: "<<endl;
@@ -73,6 +76,7 @@ int main()
         }else{
             cout<<"\"a\" al cuadrado no es igual a \"b\"";
             }*/
+
     /*quinto ejercicio*/
     /*int a;
     int contador=0;
@@ -88,23 +92,90 @@ int main()
         }else{
             cout<<a<<" es primo"<<endl;
             }*/
+
     /*sexto ejercicio*/
-    int a;
+    /*int a;
     cout<<"introduzca un numero: "<<endl;
     cin>>a;
     for(int e=2; e<a; e+=1){
-        int contador=0;
-        for (int i=2; i<=e; i+=1){
+        int contador=1;
+        for (int i=2; i<e; i+=1){
             if (e%i==0){
-                contador+=1;
+                contador=0;
                 break;
             }
         }
-        if (contador==0){
+        if (contador==1){
             cout<<e<<endl;
+            }else{
+            contador=1;
+            }
+    }*/
+
+    /*septimo ejercicio*/
+    /*cout<<"introduce un numero de 5 cifras"<<endl;
+    int a;
+    int b;
+    cin>>a;
+    for(int i=10000;i>=1;i=i/10){
+        b=a/i;
+        cout<<b<<"\t";
+        a=a%i;
+    }*/
+
+    /*octavo ejercicio*/
+     /*cout<<"introduce un numero de 5 cifras"<<endl;
+    int n,a,a1,b,b1,c,c1,d,d1;
+    cin>>n;
+    a=n/10000;
+    a1=n%10000;
+    b=a1/1000;
+    b1=a1%1000;
+    c=b1/100;
+    c1=b1%100;
+    d=c1/10;
+    d1=n%10;
+    if(a=!d1){
+        cout<<"no es palindrome";
+    }else if(b==d){
+            cout<<" es palindrome";
         }else{
-            contador=0;
-                }
+        cout<<" no es palindrome";
+        }*/
+
+    /*noveno ejercicio*/
+    /*cout<<"introduce a year: ";
+    int a;
+    cin>>a;
+    if (a%4==0){
+        cout<<"es bisiesto";
+    }else{
+    cout<<"no es bisiesto ";
+    }*/
+
+    /*decimo ejercicio*/
+    /*int a,b=1,c=0,d=0;
+    cout<<"introduzca un numero:"<<endl;
+    cin>>a;
+    for(int i=0;i<=a;i++){
+        if (i==0 or i==1){
+            cout<<i<<", ";
+        }else{
+        c=b;
+        b=b+d;
+        cout<<b<<", ";
+        d=c;
+        }
+    }*/
+
+    /*undecimo ejercicio*/
+    int a,b=1;
+    cout<<"introduzca un numero"<<endl;
+        cin>>a;
+    for(int i=1;i<=a;i++){
+            b=b*i;
     }
+    cout<<b;
+
     return 0;
 }
