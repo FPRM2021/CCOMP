557d5d68c0946c2f6cb73f720146d16f47bbbb01
Fabripink <37373367+Fabripink@users.noreply.github.com>
2018-04-20 21:02:21 -0500

    correcion de practica

--- Practica.cpp
+++ Practica.cpp
@@ -4,36 +4,46 @@ using namespace std;
 void esPerfecto(){
 for(int i=1;i<=1000;i++){
     int b=0;
-    for(int e=1;e<=i;e++){
-        if(i%e==0){
-            b=b+e;}
-        if (i==b){
+    for(int e=1;e<i;e++){
+        if(!(i%e)){
+            b=b+e;
+        }
+    }
+        if (b==i){
             for(int c=1;c<i;c++){
-            if(!(i%c)){
-            cout<<c<<" ";}}
+                if(!(b%c)){
+                    cout<<c<<" ";
+                }
+            }
             cout<<i<<endl;
-            break;}
+        }
     }
 }
-}
-void esPrimo(int a){
-int b=0;
-int contador=1;
-for(int i=2;i;i++){
-    for(int e=2;e<=i;e++){
-        if(!(i%e)){
+bool esPrimo(int a){
+    int contador=1;
+    for(int i=2;i<a;i++){
+        if(!(a%i)){
             contador++;
         }
     }
-    if(contador==2){
-        b++;
-        cout<<i<<endl;
+    if(contador==1){
+        return true;
     }else{
-    contador=1;
-    }if(a==b){
-    break;
+        return false;
     }
 }
+
+void priMen(int a){
+    int b=0;
+    for(int i=2;i;i++){
+        if(esPrimo(i)){
+            b++;
+            cout<<i<<" ";
+        }
+        if(a==b){
+            break;
+        }
+    }
 }
 
 int Pot(int a, int b){
@@ -63,5 +73,5 @@ int main()
 int a=5;
 int b=4;
 esPerfecto();
-    return 0;
+return 0;
 }
