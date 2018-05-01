e0fca7eeb440db172a08c17aafe8ec4ad3bc9856
Fabripink <37373367+Fabripink@users.noreply.github.com>
2018-05-01 13:32:30 -0500

    punteros

--- punteros.cpp
+++ punteros.cpp
@@ -11,54 +11,52 @@ int sumArray(int *arr,const int len){
 }
 
 int sumArrayR(int *arr,int len){
-    int *ptr=arr+len-1;
-         return (len==1)?ptr=arr,*ptr:*ptr+sumArrayR(arr,--len);
+    return (len==1)?*arr:*arr+sumArrayR(++arr,--len);
 }
 
 void invArr(int *arr,const int len){
-    int *ptr=(arr+len-1);
+    int *ptrf=arr+len-1;
     int a;
-    for(;ptr>=arr;ptr--,arr++){
+    for(int i=0;i<len/2;i++){
         a=*arr;
-        *arr=*ptr;
-        *ptr=a;
+        *arr=*ptrf;
+        *ptrf=a;
+        arr++;
+        ptrf--;
     }
 }
 
 int tamano(char *arr){
-    int a=0;
-    char *ptr=arr;
-    for(int i=0;*(ptr+i)!='\0';i++)
-        a++;
-    return a;
+    int c=0;
+    for(int i=0;*(arr+i)!='\0';i++)
+        c++;
+    return c;
 }
 
 void copystr(char *arr1,char *arr2){
-    char *ptr=arr1;
-    char *ptr1=arr2;
-    for(int i=0;*(ptr1+i)!='\0';i++){
-        *(ptr+i)=*(ptr1+i);
+    for(int i=0;i<tamano(arr2);i++){
+        *(arr1+i)=*(arr2+i);
     }
-    *(ptr+tamano(arr2))='\0';
+    *(arr1+tamano(arr2))='\0';
 }
 
 void concstr(char *arr1,char *arr2){
-    char *ptr=arr1;
-    char *ptr1=arr2;
-    int a;
-    for(int i=0;*(ptr+i)!='\0';i++){
-        a=i+1;
-    }
-    for(int e=0;*(ptr1+e)!='\0';e++){
-        *(ptr+a+e)=*(ptr1+e);
+    int a=tamano(arr1);
+    for(int i=0;i<tamano(arr2);i++){
+        *(arr1+a+i)=*(arr2+i);
     }
 }
+
+void printArr(int arr[],int len){
+    for(int i=0;i<len;++i){
+        cout<<arr[i]<<' ';
+    }
+}
+
 int main()
 {
     char cad[20]="hola";
     char cad1[20]="adios";
     int ar[]={1,2,3,4};
-    concstr(cad1,cad);
-    cout<<cad1;
     return 0;
 }
