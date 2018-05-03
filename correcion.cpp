40051520841bb8947ebe20dc135f3de0b126dddf
Fabripink <37373367+Fabripink@users.noreply.github.com>
2018-05-03 01:13:47 -0500

    correcion

--- correcion.cpp
+++ correcion.cpp
@@ -1,178 +1,93 @@
 #include <iostream>
 
 using namespace std;
+int lenchar(char *a,char ce='\0'){
+    int c=0;
+    for(;*(a++)!=ce;c++);
+    return c;
+}
+int pot(int b, int exp){
+    if(exp==0){
+       return 1;
+       }
+       return exp==1?b:b*pot(b,--exp);
+}
+double notacion(char* numero){
+    char tmp;
+    double n = 0;
+    int tam=lenchar(numero,'.')-1;
+    for(char* i = numero; *i!='.'; i++,tam--){
+        tmp = *i-'0';
+        n += tmp*pot(10,tam);
+    }
+    tam=lenchar(numero,'.')+1;
+    int aux = lenchar(numero,'e')-lenchar(numero,'.')-1;
+    int j = 1;
+    char *i=numero;
+    i+=tam;
+    for(;j<=aux;j++){
+        tmp = *i-'0';
+        n+=(float)tmp/pot(10,j);
+        i++;
+    }
+        tmp = numero[lenchar(numero,'e')+1];
+    int exp= numero[lenchar(numero,'e')+2]-'0';
+    if(tmp == '+')
+        return n * pot(10,exp);
+    if(tmp == '-')
+        return n/ pot(10,exp);
+}
+
+bool substring(char* s, char* t){
+    char*j=s;
+    for(char *i=t; *i!='\0';i++,j++){
+        if(*t==*s){
+            char*tmp_1 = t;
+            char*tmp_2 = s;
+            int tam = lenchar(t);
+            while(true){
+                if(*tmp_1==*tmp_2){
+                    tmp_1++;
+                    tmp_2++;
+                    --tam;
+                    if(tam==0){
+                        return true;
+                    }
+                    continue;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+char *invCad (char *cad){
+    int tam=lenchar(cad);
+    char inv[tam];
+    for (int pti=0, ptf=tam-1; pti<tam; pti++, ptf--)
+    {
+        inv[pti]=cad[ptf];
+    }
+    inv[tam] ='\0';
+    cout<<inv<<endl;
+    return inv;
+}
+
+bool esPalindromo(char *cad){
+    int tam=lenchar(cad);
+    for(int i=0; i<tam/2; i++)
+        if(cad[i] != cad[tam-i-1])
+            return false;
+    return true;
+}
 
 int main()
 {
-
-   /*primer ejercicio*/
-    /*int a;
-    cout<<"Introduzca su edad: ";
-    cin>>a;
-    if (a<18){
-        cout<<"es menor de edad";
-        }else{
-            cout<<"es mayor de edad";
-        }/*
-    /*segundo ejercicio*/
-
-    /*int n;
-    cout<<"introduzca un numero ";
-    cin>>n;
-    for (int i=1; i<=n; i++){
-        if (i!=n){
-            cout<<i<<",";
-            }else{
-            cout<<i;
-            }
-    }*/
-
-    /*tercer ejercicio*/
-    /*int a,b,c,mayor,menor;
-    cout<<"introduzca 3 numeros: "<<endl;
-    cin>>a;
-    cin>>b;
-    cin>>c;
-    menor=(a<b)?a:b;
-    menor=(menor<c?menor:c);
-    mayor=(a>b)?a:b;
-    mayor=(mayor>c?mayor:c);
-        cout<<menor<<" es el menor"<<endl;
-        cout<<mayor<<" es el mayor"<<endl;
-    int promedio=(a+b+c)/3;
-        cout<<"el promedio es: "<<promedio;
-    /*
-
-    /*cuarto ejercicio*/
-    /*int a,b;
-    cout<<"introduzca dos numeros: "<<endl;
-    cin>>a;
-    cin>>b;
-    if(!(a%2)){
-        cout<<"a es multiplo de dos"<<endl;
-        }else{
-            cout<<"a no es multiplo de dos"<<endl;
-            }
-    if(!(b%2)){
-        cout<<"b es multiplo de dos"<<endl;
-        }else{
-            cout<<"b no es multiplo de dos"<<endl;
-            }
-    if(!(a%b)){
-        cout<<"a es multiplo de b"<<endl;
-        }else{
-            cout<<"a no es multiplo de b"<<endl;
-            }
-    if (a*a==b){
-        cout<<"\"a\" al cuadrado es igual a \"b\"";
-        }else{
-            cout<<"\"a\" al cuadrado no es igual a \"b\"";
-            }*/
-
-    /*quinto ejercicio*/
-    int a;
-    int contador=0;
-    cout<<"introduzca un numero: "<<endl;
-    cin>>a;
-    for (int i=1; i<=a; i++){
-            if (!(a%i)){
-                contador++;
-        }
-    }
-    if (contador!=2){
-        cout<<a<<" no es primo"<<endl;
-        }else{
-            cout<<a<<" es primo"<<endl;
-            }
-
-    /*sexto ejercicio*/
-    /*int a;
-    cout<<"introduzca un numero: "<<endl;
-    cin>>a;
-    for(int e=2; e<a; e++){
-        int contador=1;
-        for (int i=2; i<e; i++){
-            if (!(e%i)){
-                contador=0;
-                break;
-            }
-        }
-        if (contador==1){
-            cout<<e<<endl;
-            }else{
-            contador=1;
-            }
-    }/*
-
-    /*septimo ejercicio*/
-    /*cout<<"introduce un numero de 5 cifras"<<endl;
-    int a;
-    int b;
-    cin>>a;
-    for(int i=10000;i>=1;i=i/10){
-        b=a/i;
-        cout<<b<<"\t";
-        a=a%i;
-    }*/
-
-    /*octavo ejercicio*/
-     /*cout<<"introduce un numero de 5 cifras"<<endl;
-    int n,a,a1,b,b1,c,c1,d,d1;
-    cin>>n;
-    a=n/10000;
-    a1=n%10000;
-    b=a1/1000;
-    b1=a1%1000;
-    c=b1/100;
-    c1=b1%100;
-    d=c1/10;
-    d1=n%10;
-    if(a=!d1){
-        cout<<"no es palindrome";
-    }else if(b==d){
-            cout<<" es palindrome";
-        }else{
-        cout<<" no es palindrome";
-        }*/
-
-    /*noveno ejercicio*/
-    /*cout<<"introduce un ahno: ";
-    int a;
-    cin>>a;
-    if (!(a%4) or !(a%400)){
-            if(a%100 or !(a%4 or a%400)){
-            cout<<"es bisiesto";
-            }else{
-            cout<<"no es bisiesto";
-            }
-    }else {
-    cout<<"no es bisiesto";
-    }/*
-
-    /*decimo ejercicio*/
-    /*int a,b=1,c=0,d=0;
-    cout<<"introduzca un numero:"<<endl;
-    cin>>a;
-    for(int i=0;i<=a;i++){
-        if (i==0 or i==1){
-            cout<<i<<", ";
-        }else{
-        c=b;
-        b=b+d;
-        cout<<b<<", ";
-        d=c;
-        }
-    }*/
-
-    /*undecimo ejercicio*/
-    /*int a,b=1;
-    cout<<"introduzca un numero"<<endl;
-        cin>>a;
-    for(int i=1;i<=a;i++){
-            b=b*i;
-    }
-    cout<<b;*/
-
+    char n[]={"123.456e+6"};
+    char cd[]={"12321"};
+    cout<<esPalindromo(cd);
     return 0;
 }
-
