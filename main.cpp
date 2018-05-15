6a0456483df57f8e9a4617d507caa45d4afee2d9
Fabripink <37373367+Fabripink@users.noreply.github.com>
2018-05-15 09:55:43 -0500

    POO

--- main.cpp
+++ main.cpp
@@ -1,131 +1,57 @@
- #include <iostream>
+#include <iostream>
+#include <POO.h>
 
 using namespace std;
 
-int main()
-{
-    /*int x=8;
-    float y=3;
-    int s=x+y;
-    int r=x-y;
-    int m=x*y;
-    float d=x/y;
-    int a,b;
-    std::cout <<"el primer numero es: "<<x<<std::endl;
-    std::cout <<"el segundo numero es: "<<y<<std::endl;
-    std::cout <<"la suma es: "<<s<<std::endl;
-    std::cout <<"la resta es: "<<r<<std::endl;
-    std::cout <<"la multiplicacion es: "<<m<<std::endl;
-    std::cout <<"la division es: "<<d<<std::endl;
-    //std::cout <<"a es: "<<a<<" y b es: "<<b<<std::endl;
-    std::cout <<"ingrese el radio del circulo: "<<std::endl;
-    cin>>a;
-    float c=(3.14)*(a*a);
-    cout<<"el area es: "<<c;
-    cout<<"*********\t **\t\t*\t\t*\n*\t*      *    *\t       ***\t       * *\n*\t*     *      *\t      *****\t      *   *\n*\t*     *      *\t     *******\t     *     *\n*\t*      *    *\t        *\t    *       *\n*********\t **\t\t*\t   ***********";*/
-    /*int p=7;
-    int r=2;
-    int q=6;
-    int w=4;
-    int x=5;
-    int y=2;
-    int z=p*r%q+w/x-y;*/
-    /*cin>>a;
-    cin>>b;
-    cin>>c;
-    if (a>b and a>c){
-        cout<<a;
-    }else{
-    if(b>a and b>c){
-        cout<<b;
-    }else{
-        cout<<c;
-    }
-    }*/
-
-    /*int a;
-    int b;
-    int c;
-    int d;
-    int e;
-    cin>>a;
-    cin>>b;
-    cin>>c;
-    cin>>d;
-    cin>>e;
-    if (a>b and a>c and a>d and a>e){
-        cout<<a;
-    }else{
-        if(b>a and b>c and b>d and b>e){
-        cout<<b;}
-        else{
-        if(c>a and c>b and c>d and c>e){
-        cout<<c;}
-        else{
-        if(d>a and d>b and d>c and d>e){
-        cout<<d;}
-        else{
-        if(e>a and e>b and e>c and e>d){
-        cout<<e;}}}}}
-        if (a>b and a>c and a>d and a>e){
-        cout<<" "<<a;
-    }else{
-        if(b<a and b<c and b<d and b<e){
-        cout<<" "<<b;}
-        else{
-        if(c<a and c<b and c<d and c<e){
-        cout<<" "<<c;}
-        else{
-        if(d<a and d<b and d<c and d<e){
-        cout<<" "<<d;}
-        else{
-        if(e<a and e<b and e<c and e<d){
-        cout<<" "<<e;}}}}}*/
-    /*int n;
-    cin>>n;
-    if (n%2){
-        cout<<"impar";
-    }else{
-    cout<<"par";
-    }
-    int a;
-    int b;
-    cin>>a;
-    cin>>b;
-    if (a%b){
-        cout<<"no es multiplo";
-    }else{
-        cout<<"si es multiplo";
-    }
-    int x;
-    cin>>x;
-
-    for(int i=10000;i>=1;i=i/10){
-        int tmp=x/i;
-        cout<<tmp<<" ";
-        x=x%i;
-    }*/
-
-    int mayor=0;
-    int menor=0;
-    int suma=0;
-    int a;
-    int n;
-    float promedio=0;
-    cin>>n;
-    cin>>a;
-    mayor=a;
-    menor=a;
-    for(int i=1; i<n; i=i+1){
-        cin>>a;
-        if (a>mayor){
-            mayor=a;
-        }else if(a<menor){
-            menor=a;
-        suma+=a;
+void ordbur(POO cursos[],int len){
+    for(int i=0; i<len;i++){
+        for(int j=i+1;j<len;j++){
+            if(cursos[i].promedio>cursos[j].promedio){
+                int aux=cursos[i].promedio;
+                cursos[i].promedio=cursos[j].promedio;
+                cursos[j].promedio=aux;
+            }
         }
+
     }
-    promedio=suma/n;
-    cout<<"promedio: "<<promedio<<" mayor: "<<mayor<<" menor: "<<menor<<endl;
+}
+
+void impcad(POO cursos[],int len){
+    for(int i=0;i<len;i++){
+        cout<<cursos[i].promedio<<" ";
+    }
+}
+
+void ordburc(POO cursos[]){
+    for(int i=0;cursos[i].nombre=!'\0';i++){
+        for(int j=i+1;cursos[j].nombre=!'\0';j++){
+            if(cursos[i].promedio>cursos[j].promedio){
+                int aux=cursos[i].promedio;
+                cursos[i].promedio=cursos[j].promedio;
+                cursos[j].promedio=aux;
+            }
+        }
+
+    }
+}
+
+
+int main(){
+    POO cursos[5];
+    cursos[0].nombre="Ciencia de la Computación";
+    cursos[0].promedio=18;
+    cursos[1].nombre="Matematica II";
+    cursos[1].promedio=11;
+    cursos[2].nombre="ICC";
+    cursos[2].promedio=17;
+    cursos[3].nombre="Discretas II";
+    cursos[3].promedio=16;
+    cursos[4].nombre="IVU";
+    cursos[4].promedio=14;
+
+    //ordbur(cursos,5);
+    //impcad(cursos,5);
+
+
     return 0;
 }
