b4e7ea8d097df0e2a3cefa6240f373813f542bd6
Fabripink <37373367+Fabripink@users.noreply.github.com>
2018-07-02 11:28:45 -0500

    PRACTICA02/07/18

--- Practica/include/Widget.h
+++ Practica/include/Widget.h
@@ -3,95 +3,158 @@
 #include <iostream>
 using namespace std;
 
-class Widget
-{
-    static int numInstances;
-    public:
-        Widget(){numInstances++;}
-        ~Widget(){numInstances--;}
-        static int getNumInstances(){return numInstances;}
-            virtual int f(){return 1;}
-};
+////class UCSPStudent{
+////public:
+////    char *nombre;
+////    int codigo;
+////    UCSPStudent(){
+////        codigo=0;
+////        nombre="";
+////    }
+////    UCSPStudent(UCSPStudent &o){
+////    codigo=o.codigo;
+////    nombre=new char;
+////    for(int i=0;i<'\0';i++)
+////        nombre[i]=o.nombre[i];
+////    }
+////};
 
-int Widget::numInstances=0;
+////class Widget
+////{
+////    static int numInstances;
+////    public:
+////        Widget(){numInstances++;}
+////        ~Widget(){numInstances--;}
+////        static int getNumInstances(){return numInstances;}
+////            virtual int f(){return 1;}
+////};
+////
+////int Widget::numInstances=0;
+////
+////class Gadget: public    Widget{
+////    public:
+////         virtual int f(){return 2;}
+////};
+////
+////class Gizmo: public    Widget{
+////    public:
+////         virtual int f(){return 3;}
+////};
+////
+////void do_it(Widget *w){
+////    cout<<w->f()<<" ";
+////}
+////
+////class ArrayWidgets{
+////    Widget *items;
+////    int tam;
+////    void resize(int tam){this->tam=tam;}
+////public:
+////    ArrayWidgets(int tam){//constructor1
+////        this->tam=tam;
+////        items=new Widget[tam];
+////    }
+////
+////    ArrayWidgets(const Widget wids[], const int tam){//constructor2
+////        items=new Widget[tam];
+////        for(int i=0;i<tam;i++)
+////            items[i]=wids[i];
+////        this->tam=tam;
+////        delete []wids;
+////    }
+////    ArrayWidgets(const ArrayWidgets &arr){//constructor copia
+////        tam=arr.tam;
+////        items=new Widget[tam];
+////        for(int i=0;i<tam;i++)
+////            items[i]=arr.items[i];
+////    }
+////    ~ArrayWidgets(){
+////        delete []items;
+////    }
+////    int getTam(){
+////        return tam;
+////    }
+////    void push_back(const Widget &w){
+////        Widget *nuevo=new Widget[tam+1];
+////        for(int i=0;i<tam;i++)
+////            nuevo[i]=items[i];
+////        nuevo[tam]=w;
+////        tam++;
+////        delete []items;
+////        items=nuevo;
+////    }
+////
+////    void insert(const int pos, const Widget &w){
+////        Widget *nuevo=new Widget [tam+1];
+////        for(int i=0;i<pos;i++)
+////            nuevo[i]=items[i];
+////        nuevo[pos]=w;
+////        for(int i=pos;i<tam;i++)
+////            nuevo[i+1]=items[i];
+////        tam++;
+////        delete []items;
+////        items=nuevo;
+////    }
+////
+////    void remove(const int pos){
+////        Widget *nuevo=new Widget[tam-1];
+////        for(int i=0;i<pos;i++)
+////            nuevo[i]=items[i];
+////        for(int i=pos;i<tam-1;i++)
+////            nuevo[i]=items[i+1];
+////        tam--;
+////        delete []items;
+////        items=nuevo;
+////    }
+////};
 
-class Gadget: public    Widget{
-    public:
-         virtual int f(){return 2;}
-};
-
-class Gizmo: public    Widget{
-    public:
-         virtual int f(){return 3;}
-};
-
-void do_it(Widget *w){
-    cout<<w->f()<<" ";
-}
-
-class ArrayWidgets{
-    Widget *items;
-    int tam;
-    void resize(int tam){this->tam=tam;}
+class Animal{
+char *nombre;
+int patas;
 public:
-    ArrayWidgets(int tam){//constructor1
-        this->tam=tam;
-        items=new Widget[tam];
+    Animal(char *n){
+        nombre=n;
+        patas=4;
     }
+    virtual char *hablar();
+};
 
-    ArrayWidgets(const Widget wids[], const int tam){//constructor2
-        items=new Widget[tam];
-        for(int i=0;i<tam;i++)
-            items[i]=wids[i];
-        this->tam=tam;
-        delete []wids;
-    }
-    ArrayWidgets(const ArrayWidgets &arr){//constructor copia
-        tam=arr.tam;
-        items=new Widget[tam];
-        for(int i=0;i<tam;i++)
-            items[i]=arr.items[i];
-    }
-    ~ArrayWidgets(){
-        delete []items;
-    }
-    int getTam(){
-        return tam;
-    }
-    void push_back(const Widget &w){
-        Widget *nuevo=new Widget[tam+1];
-        for(int i=0;i<tam;i++)
-            nuevo[i]=items[i];
-        nuevo[tam]=w;
-        tam++;
-        delete []items;
-        items=nuevo;
-    }
-
-    void insert(const int pos, const Widget &w){
-        Widget *nuevo=new Widget [tam+1];
-        for(int i=0;i<pos;i++)
-            nuevo[i]=items[i];
-        nuevo[pos]=w;
-        for(int i=pos;i<tam;i++)
-            nuevo[i+1]=items[i];
-        tam++;
-        delete []items;
-        items=nuevo;
-    }
-
-    void remove(const int pos){
-        Widget *nuevo=new Widget[tam-1];
-        for(int i=0;i<pos;i++)
-            nuevo[i]=items[i];
-        for(int i=pos;i<tam-1;i++)
-            nuevo[i]=items[i+1];
-        tam--;
-        delete []items;
-        items=nuevo;
+class Perro:public Animal{
+public:
+    Perro(char *n):Animal(n){}
+    char *hablar(){
+        return "wuff wuff";
     }
 };
 
+class Gato:public Animal{
+public:
+    Gato(char *n):Animal(n){}
+    char *hablar(){
+        return "miau";
+    }
+};
 
+class Mascotas{
+Animal **an;
+int tam=1;
+public:
+    Mascotas(Animal *a){
+        an=new Animal*[tam];
+        an[0]=a;
+    }
+    void Insertar(Animal *a){
+        Animal **nuevo=new Animal*[tam+1];
+        for(int i=0;i<tam;i++)
+            nuevo[i]=an[i];
+        nuevo[tam]=a;
+        delete []an;
+        an=nuevo;
+    }
+    void hablar(){
+        for(int i=0;i<tam;i++)
+            cout<<an[i]->hablar()<<endl;
+    }
+};
 
 #endif // WIDGET_H
--- Practica/main.cpp
+++ Practica/main.cpp
@@ -4,36 +4,57 @@
 
 using namespace std;
 
-int* retornaMemoria(int a){
-    int *ptr=&a;
-    return ptr;
-}
-
-int Mayor(int a, int b, int c){
-    int mayor=a>b?a>c?a:c:b>c?b:c;
-    return mayor;
-}
+////int* retornaMemoria(int a){
+////    int *ptr=&a;
+////    return ptr;
+////}
+////
+////int Mayor(int a, int b, int c){
+////    int mayor=a>b?a>c?a:c:b>c?b:c;
+////    return mayor;
+////}
 
 int main()
 {
 ////    int a=5;
 ////    cout<<retornaMemoria(a);
 
-  vector<Widget *> widgets;
-  Widget wid;//un objeto Widget
-  Gadget gad;//segundo
-  Gizmo giz;//tercer
-  if(true){
-    Gizmo giz2;//4to dentro del ambito IF
-    cout<<wid.getNumInstances()<<endl;
-  }
-  cout<<wid.getNumInstances()<<endl; //
-  widgets.push_back(&wid);
-  widgets.push_back(&gad);
-  widgets.push_back(&giz);
-  for(int i=0; i<widgets.size();i++)
-    do_it(widgets[i]); //
-    cout<<endl;
-    cout<<Mayor(8,9,10);
+////  vector<Widget *> widgets;
+////  Widget wid;//un objeto Widget
+////  Gadget gad;//segundo
+////  Gizmo giz;//tercer
+////  if(true){
+////    Gizmo giz2;//4to dentro del ambito IF
+////    cout<<wid.getNumInstances()<<endl;
+////  }
+////  cout<<wid.getNumInstances()<<endl; //
+////  widgets.push_back(&wid);
+////  widgets.push_back(&gad);
+////  widgets.push_back(&giz);
+////  for(int i=0; i<widgets.size();i++)
+////    do_it(widgets[i]); //
+////    cout<<endl;
+////    cout<<Mayor(8,9,10);
+
+//    UCSPStudent estudiante1;
+//    estudiante1.codigo=123456;
+//    char nombre[]="Carlos";
+//    estudiante1.nombre=nombre;
+//    if(true){
+//        UCSPStudent estudiante2=estudiante1;
+//        estudiante2.nombre[2]='j';
+//        cout<<estudiante1.nombre<<endl;
+//    }
+    Animal *p,*g;
+    p=new Perro("Ramiro");
+    g=new Gato("Lancelot");
+
+    cout<< p->hablar()<<endl;
+    cout<< g->hablar()<<endl;
+
+    Mascotas m(p);
+    m.Insertar(g);
+    m.hablar();
+
     return 0;
 }
